e96377fa0ff7a907884b21629a30f33d3e6ed52a
Carson <carson608@gmail.com>
2018-09-20 18:18:21 -0700

    changed knexfile.js

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -4,3 +4,35 @@ module.exports = {
     connection: 'postgres://axonuakyjzsquy:240b535d06397d869b30b9fb2e8eedafe06d2ba6dfd90284342290e45338b6e8@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d902q0l08ldcqs',
     ssl: true
 }
+
+module.exports = {
+  dev: {
+    client: 'postgresql',
+    connection: 'postgres://axonuakyjzsquy:240b535d06397d869b30b9fb2e8eedafe06d2ba6dfd90284342290e45338b6e8@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d902q0l08ldcqs',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/seeds'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: 'postgres://axonuakyjzsquy:240b535d06397d869b30b9fb2e8eedafe06d2ba6dfd90284342290e45338b6e8@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d902q0l08ldcqs',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/seeds'
+    }
+  },
+  ssl: true
+};
