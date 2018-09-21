8a3a7becb08854e15b0c8be3291acaa6c3cf40fc
Carson <carson608@gmail.com>
2018-09-20 18:29:28 -0700

    changed knexfile.js

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,19 +1,42 @@
 module.exports = {
   dev: {
     client: 'postgresql',
-    connection: 'postgres://axonuakyjzsquy:240b535d06397d869b30b9fb2e8eedafe06d2ba6dfd90284342290e45338b6e8@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d902q0l08ldcqs',
+    connection: {
+      port: process.env.BALANCE_DB_PORT,
+      host: process.env.BALANCE_DB_HOST,
+      database: process.env.BALANCE_DB_NAME,
+      user: process.env.BALANCE_DB_USER,
+      password: process.env.BALANCE_DB_PW
+    },
     pool: {
       min: 2,
       max: 10
+    },
+    migrations: {
+      directory: __dirname + '/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/seeds'
     }
   },
   production: {
     client: 'postgresql',
-    connection: 'postgres://axonuakyjzsquy:240b535d06397d869b30b9fb2e8eedafe06d2ba6dfd90284342290e45338b6e8@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d902q0l08ldcqs',
+    connection: process.env.DATABASE_URL ? process.env.DATABASE_URL : {
+      port: process.env.BALANCE_DB_PORT_PROD,
+      host: process.env.BALANCE_DB_HOST_PROD,
+      database: process.env.BALANCE_DB_NAME_PROD,
+      user: process.env.BALANCE_DB_USER_PROD,
+      password: process.env.BALANCE_DB_PW_PROD
+    },
     pool: {
       min: 2,
       max: 10
+    },
+    migrations: {
+      directory: __dirname + '/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/seeds'
     }
-  },
-  ssl: true
+  }
 };
