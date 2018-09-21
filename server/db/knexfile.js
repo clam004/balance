d64eb03f392264033def87e5d3ffbabc0605e8ac
Carson <carson608@gmail.com>
2018-09-20 18:14:07 -0700

    changed knexfile.js

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -2,8 +2,5 @@ module.exports = {
     client: 'pg',
     debug: true,
     connection: 'postgres://axonuakyjzsquy:240b535d06397d869b30b9fb2e8eedafe06d2ba6dfd90284342290e45338b6e8@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d902q0l08ldcqs',
-    migrations: {
-        tableName: 'migrations'
-    },
     ssl: true
 }
