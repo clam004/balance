9b65fa9357d3940b2398cad417f34e63a6f7d4d3
Carson <carson608@gmail.com>
2018-09-29 16:28:07 -0700

    made a table called balances and used it to fill the dashboard

---
+++ server/db/migrations/20180929161639_create_user.js
@@ -0,0 +1,16 @@
+exports.up = (knex, Promise) => {
+
+  return knex.schema.createTable('users', table => {
+    table.increments('id');
+    table.string('email').notNullable();
+    table.string('password').notNullable();
+    table.string('salt').notNullable();
+    table.string('username');
+  })
+  
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('users');
+};
+
